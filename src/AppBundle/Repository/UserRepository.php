f67ef7e9664ea0efaa2dd0079942ef8d290bcb19
black-bullet <blackbullet@i.ua>
2016-01-14 17:49:08 +0200

    Fix annotation

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class UserRepository
+ *
+ * @author Yevgeniy Zholkevskiy <blackbullet@i.ua>
+ */
+class UserRepository extends EntityRepository
+{
+}
