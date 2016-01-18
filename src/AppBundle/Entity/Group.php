9e9e065c629a9dbee8a314cdf34a09803d7b1756
black-bullet <blackbullet@i.ua>
2016-01-18 11:00:42 +0200

    Create new entity Group

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use AppBundle\Entity\BlameableEntityTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Group Entity
+ *
+ * @author Yevgeniy Zholkevskiy <blackbullet@i.ua>
+ *
+ * @ORM\Table(name="groups")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GroupRepository")
+ */
+class Group
+{
+    use TimestampableEntity;
+
+    use BlameableEntityTrait;
+
+    /**
+     * @var int $id ID
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name Name
+     *
+     * @ORM\Column(type="string", length=100, nullable=false)
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", max="100")
+     * @Assert\Type(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string $description Description
+     *
+     * @ORM\Column(type="text", nullable=true)
+     *
+     * @Assert\Type(type="string")
+     */
+    private $description;
+
+    /**
+     * @var int $foundedAt Year foundation
+     *
+     * @ORM\Column(type="integer", nullable=true)
+     *
+     * @Assert\Type(type="integer")
+     */
+    private $foundedAt;
+
+    /**
+     * Get ID
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name Name
+     *
+     * @return Group
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string Name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description Description
+     *
+     * @return Group
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string Description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set year foundation
+     *
+     * @param int $foundedAt Year foundation
+     *
+     * @return Group
+     */
+    public function setFoundedAt($foundedAt)
+    {
+        $this->foundedAt = $foundedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get founded At
+     *
+     * @return \DateTime Founded At
+     */
+    public function getFoundedAt()
+    {
+        return $this->foundedAt;
+    }
+}
