39132e64284d3efa4a332dcb928155647bf3a296
black-bullet <blackbullet@i.ua>
2016-01-21 16:19:32 +0200

    Customize login and registration

---
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email','email', ['attr' => ['class' => 'form-control']])
+                ->add('username', 'text', ['attr' => ['class' => 'form-control']])
+                ->add('plainPassword', 'password', ['attr' => ['class' => 'form-control']]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+}
