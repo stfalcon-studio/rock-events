ba78370bf897719eb7e686f2102438a7b2f1d9d3
black-bullet <blackbullet@i.ua>
2016-01-21 16:22:02 +0200

    Create user groups bookmark

--- src/AppBundle/Controller/Frontend/GroupController.php
+++ src/AppBundle/Controller/Frontend/GroupController.php
@@ -3,11 +3,14 @@
 namespace AppBundle\Controller\Frontend;
 
 use AppBundle\Entity\Group;
+use AppBundle\Entity\UserGroup;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Frontend GroupController
@@ -21,15 +24,23 @@ class GroupController extends Controller
      *
      * @return Response
      *
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      * @Route("/groups", name="group_list")
      */
     public function listAction()
     {
         $groups = $this->getDoctrine()->getRepository('AppBundle:Group')->findAll();
+        if(null===$this->getUser()) {
+            return $this->render('AppBundle:frontend\group:list.html.twig', [
+                'groups' => $groups
+            ]);
+        }
+
+        $userGroups = $this->getDoctrine()->getRepository('AppBundle:Group')->findGroupsByUser($this->getUser());
 
         return $this->render('AppBundle:frontend\group:list.html.twig', [
-            'groups' => $groups,
+            'groups'     => $groups,
+            'userGroups' => $userGroups
         ]);
     }
 
@@ -73,5 +84,55 @@ class GroupController extends Controller
             'events' => $events,
             'group'  => $group
         ]);
+    }
+
+    /**
+     * @param Group $group
+     *
+     * @Route("/group/{slug}/bookmark", name="group_add_to_bookmark")
+     * @ParamConverter("group", class="AppBundle:Group")
+     *
+     * @return RedirectResponse
+     */
+    public function addToBookmarkAction(Group $group)
+    {
+        if (null === $this->getUser()) {
+            throw new HttpException(401, "Forbidden");
+        }
+
+        $user      = $this->getUser();
+        $userGroup = (new UserGroup())->setUser($user)->setGroup($group);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($userGroup);
+        $em->flush();
+
+        return $this->redirectToRoute("group_list");
+    }
+
+    /**
+     * @param Group $group
+     *
+     * @Route("/group/{slug}/bookmark/delete", name="group_delete_from_bookmark")
+     * @ParamConverter("group", class="AppBundle:Group")
+     *
+     * @return RedirectResponse
+     */
+    public function deleteFromBookmarkAction(Group $group)
+    {
+        if (null === $this->getUser()) {
+            throw new HttpException(401, "Forbidden");
+        }
+
+        $userGroup = $this->getDoctrine()->getRepository('AppBundle:UserGroup')->findOneBy([
+            'user' => $this->getUser(),
+            'group' => $group
+        ]);
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($userGroup);
+        $em->flush();
+
+        return $this->redirectToRoute("group_list");
     }
 }
