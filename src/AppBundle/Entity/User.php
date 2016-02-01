40a607283fd843c907c6496120883b0828dfba9d
black-bullet <blackbullet@i.ua>
2016-02-01 10:48:01 +0200

    Remove relation ticket with user and add field linkToBuyTicket for ticket

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -46,13 +46,6 @@ class User extends BaseUser
     private $userGroups;
 
     /**
-     * @var ArrayCollection|Ticket[] $tickets Ticket
-     *
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Ticket", mappedBy="user")
-     */
-    private $tickets;
-
-    /**
      * @var ArrayCollection|ManagerGroup[] $mangerGroups Manager Groups
      *
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\ManagerGroup", mappedBy="manager")
@@ -142,33 +135,6 @@ class User extends BaseUser
     public function getUserGroups()
     {
         return $this->userGroups;
-    }
-
-    /**
-     * Set Ticket
-     *
-     * @param ArrayCollection|Ticket[] $tickets Ticket
-     *
-     * @return $this
-     */
-    public function setTickets(ArrayCollection $tickets)
-    {
-        foreach ($tickets as $ticket) {
-            $ticket->setUser($this);
-        }
-        $this->tickets = $tickets;
-
-        return $this;
-    }
-
-    /**
-     * Get tickets
-     *
-     * @return ArrayCollection|Ticket[] Tickets
-     */
-    public function getTickets()
-    {
-        return $this->tickets;
     }
 
     /**
